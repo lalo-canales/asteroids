7dd380a6605559898de44f3bb9a3a43124e3480b
lalo_canales <canales.lalo@gmail.com>
2026-04-25 09:11:25 -0700

    added game_clock and dt calc

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -3,19 +3,24 @@ from constants import *
 from logger import log_state
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids with pygame version: 2.6.1")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	print("Starting Asteroids with pygame version: 2.6.1")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
 
-    while True:
-    	log_state()
-    	for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-    	screen.fill("black")
-    	pygame.display.flip()
+	game_clock = pygame.time.Clock()
+	dt = 0
+	while True:
+		log_state()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+
+		screen.fill("black")
+		pygame.display.flip()
+		delta_time = game_clock.tick(60)
+		dt = delta_time/1000
         
 if __name__ == "__main__":
     main()
