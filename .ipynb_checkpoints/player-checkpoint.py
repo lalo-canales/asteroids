f79de28f6d4b61d54f52cd701970fc8f7037891e
lalo_canales <canales.lalo@gmail.com>
2026-05-04 13:53:00 -0700

    added shot cooldown

--- .ipynb_checkpoints/player-checkpoint.py
+++ .ipynb_checkpoints/player-checkpoint.py
@@ -6,6 +6,7 @@ class Player(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
+		self.shot_timer = 0
 
 	# in the Player class
 	def triangle(self):
@@ -20,6 +21,7 @@ class Player(CircleShape):
 		self.rotation += PLAYER_TURN_SPEED * dt
 
 	def update(self, dt):
+		self.shot_timer -= dt
 		keys = pygame.key.get_pressed()
 		if keys[pygame.K_a]:
 			self.rotate(-dt)
@@ -39,6 +41,10 @@ class Player(CircleShape):
 		self.position += rotated_with_speed_vector
 
 	def shoot(self):
-		bullet = Shot(self.position.x, self.position.y)
-		bullet.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+		if self.shot_timer > 0:
+			pass
+		else:
+			bullet = Shot(self.position.x, self.position.y)
+			bullet.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+			self.shot_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
 		
